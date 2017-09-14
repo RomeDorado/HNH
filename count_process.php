a20a1e085b821cfc3a779d8acc79d225c6a9d074
RomeDorado <romedorado@gmail.com>
2017-09-14 23:09:05 +0800

    all

--- count_process.php
+++ count_process.php
@@ -5,24 +5,25 @@ $connect = mysqli_connect("localhost", "root", "", "healthy_corner");
 
 $sql = "SELECT * FROM product_mother";
 $result = mysqli_query($connect, $sql);
-
 $val = $_POST['btn'];
 $amount = $_POST["amount"];
+$product = $_POST["ingr"];
 if($_POST["amount"] != 0 && $_POST["amount"] > 0){
+
 if($val == 'Add'){
 
-
+$empquery = "INSERT INTO `consumed_logs` (`id`, `product`, `date`,`amount`, `employee_name`) VALUES (null, '$product', current_timestamp,'$amount' ,0);";
 $newquery = "UPDATE product_mother SET employee_count ='$amount' WHERE ID='$_POST[id]'";
 
 mysqli_query($connect, $newquery);
+mysqli_query($connect, $empquery);
+
+
+	}
+	
 	
 
-
-        }
-	
-	
-
-
+}
 $connect->close();
 header('Location: ' . $_SERVER['HTTP_REFERER']);
   
